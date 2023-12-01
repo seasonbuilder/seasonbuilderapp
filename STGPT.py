bc0918b5b457ffd1755c66148169b3df055ccf86
seasonbuilder <taylor@seasonbuilder.org>
2023-11-30 22:50:42 -0600

    Update STGPT.py

--- STGPT.py
+++ STGPT.py
@@ -13,6 +13,18 @@ client = OpenAI()
 # Your chosen model
 MODEL = "gpt-4-1106-preview"
 
+# Set up the page
+st.set_page_config(page_title="Coach Aidge - Virtual Life Coach")
+header = st.container()
+col1,col2 = st.columns(2)
+
+with header:
+    with col1:
+        st.image("https://static.wixstatic.com/media/b748e0_3d5d304967484a6db8794389ab2d2df5~mv2.png")
+    with col2:
+        st.header("Coach Aidge")
+        st.caption("Your Virtual Life Coach Trained by Season Builder")
+
 # Initialize session state variables
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
@@ -26,88 +38,73 @@ if "messages" not in st.session_state:
 if "retry_error" not in st.session_state:
     st.session_state.retry_error = 0
 
-# Set up the page
-st.set_page_config(page_title="Coach Aidge - Virtual Life Coach")
-header = st.container()
-col1,col2 = st.columns(2)
-body = st.container()
+# Initialize OpenAI assistant
+if "assistant" not in st.session_state:
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT"])
+    st.session_state.thread = client.beta.threads.create(
+        metadata={'session_id': st.session_state.session_id}
+    )
 
+# Display chat messages
+elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == "completed":
+    st.session_state.messages = client.beta.threads.messages.list(
+        thread_id=st.session_state.thread.id
+    )
+    for message in reversed(st.session_state.messages.data):
+        if message.role in ["user", "assistant"]:
+            with st.chat_message(message.role):
+                for content_part in message.content:
+                    message_text = content_part.text.value
+                    st.markdown(message_text)
 
-with header:
-    with col1:
-        st.image("https://static.wixstatic.com/media/b748e0_3d5d304967484a6db8794389ab2d2df5~mv2.png")
-    with col2:
-        st.header("Coach Aidge")
-        st.caption("Your Virtual Life Coach Trained by Season Builder")
+# Chat input and message creation
 
-with body:
-    # Initialize OpenAI assistant
-    if "assistant" not in st.session_state:
-        openai.api_key = st.secrets["OPENAI_API_KEY"]
-        st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT"])
-        st.session_state.thread = client.beta.threads.create(
-            metadata={'session_id': st.session_state.session_id}
-        )
+if prompt := st.chat_input("How can I help you?"):
+    with st.chat_message('user'):
+        st.write(prompt)
 
-    # Display chat messages
-    elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == "completed":
-        st.session_state.messages = client.beta.threads.messages.list(
-            thread_id=st.session_state.thread.id
-        )
-        for message in reversed(st.session_state.messages.data):
-            if message.role in ["user", "assistant"]:
-                with st.chat_message(message.role):
-                    for content_part in message.content:
-                        message_text = content_part.text.value
-                        st.markdown(message_text)
+    message_data = {
+        "thread_id": st.session_state.thread.id,
+        "role": "user",
+        "content": prompt
+    }
 
-    # Chat input and message creation
+   
+    st.session_state.messages = client.beta.threads.messages.create(**message_data)
 
-    if prompt := st.chat_input("How can I help you?"):
-        with st.chat_message('user'):
-            st.write(prompt)
+    st.session_state.run = client.beta.threads.runs.create(
+        thread_id=st.session_state.thread.id,
+        assistant_id=st.session_state.assistant.id,
+    )
+    if st.session_state.retry_error < 3:
+        time.sleep(1)
+        st.rerun()
 
-        message_data = {
-            "thread_id": st.session_state.thread.id,
-            "role": "user",
-            "content": prompt
-        }
-
-       
-        st.session_state.messages = client.beta.threads.messages.create(**message_data)
-
-        st.session_state.run = client.beta.threads.runs.create(
-            thread_id=st.session_state.thread.id,
-            assistant_id=st.session_state.assistant.id,
-        )
+# Handle run status
+if hasattr(st.session_state.run, 'status'):
+    if st.session_state.run.status == "in progress":
+        with st.chat_message('assistant'):
+            st.write("Thinking ......")
         if st.session_state.retry_error < 3:
             time.sleep(1)
             st.rerun()
 
-    # Handle run status
-    if hasattr(st.session_state.run, 'status'):
-        if st.session_state.run.status == "in progress":
-            with st.chat_message('assistant'):
-                st.write("Thinking ......")
+    elif st.session_state.run.status == "failed":
+        st.session_state.retry_error += 1
+        with st.chat_message('assistant'):
             if st.session_state.retry_error < 3:
-                time.sleep(1)
-                st.rerun()
-
-        elif st.session_state.run.status == "failed":
-            st.session_state.retry_error += 1
-            with st.chat_message('assistant'):
-                if st.session_state.retry_error < 3:
-                    st.body.write("Run failed, retrying ......")
-                    time.sleep(3)
-                    st.rerun()
-                else:
-                    st.body.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
-
-        elif st.session_state.run.status != "completed":
-            st.session_state.run = client.beta.threads.runs.retrieve(
-                thread_id=st.session_state.thread.id,
-                run_id=st.session_state.run.id,
-            )
-            if st.session_state.retry_error < 3:
+                st.body.write("Run failed, retrying ......")
                 time.sleep(3)
                 st.rerun()
+            else:
+                st.body.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
+
+    elif st.session_state.run.status != "completed":
+        st.session_state.run = client.beta.threads.runs.retrieve(
+            thread_id=st.session_state.thread.id,
+            run_id=st.session_state.run.id,
+        )
+        if st.session_state.retry_error < 3:
+            time.sleep(3)
+            st.rerun()
