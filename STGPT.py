fe4012c019c84e89cd8c4941a1baf211b23194e1
seasonbuilder <taylor@seasonbuilder.org>
2023-12-02 14:29:01 -0600

    Update STGPT.py

--- STGPT.py
+++ STGPT.py
@@ -82,7 +82,7 @@ if prompt := st.chat_input("How can I help you?"):
 
 # Handle run status
 if hasattr(st.session_state.run, 'status'):
-    if st.session_state.run.status == "in_progress":
+    if st.session_state.run.status == "in progress":
         with st.chat_message('assistant'):
             st.write("Thinking ......")
         if st.session_state.retry_error < 3:
