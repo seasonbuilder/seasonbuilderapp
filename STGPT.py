3cee53e2ea2c4138b1013e1b6e52808e5b4c4d01
seasonbuilder <taylor@seasonbuilder.org>
2023-12-09 13:27:08 -0600

    Update STGPT.py

--- STGPT.py
+++ STGPT.py
@@ -130,7 +130,7 @@ st.markdown("""
         <style>
                .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 1rem;
+                    padding-bottom: 3rem;
                     padding-left: 1rem;
                     padding-right: 1rem;
                 }
