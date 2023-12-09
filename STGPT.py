459516896226fd951599ac635554730e5a625e65
seasonbuilder <taylor@seasonbuilder.org>
2023-12-09 13:22:18 -0600

    Update STGPT.py

--- STGPT.py
+++ STGPT.py
@@ -24,16 +24,7 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-st.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 3rem;
-                    padding-left: 1rem;
-                    padding-right: 1rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
+
 
 col1,col2 = st.columns([1,3])
 header = st.container()
@@ -134,3 +125,14 @@ if hasattr(st.session_state.run, 'status'):
         if st.session_state.retry_error < 3:
             time.sleep(3)
             st.rerun()
+
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 1rem;
+                    padding-left: 1rem;
+                    padding-right: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
