11deffe6150194e76d6e9f76a812095483139ca3
jtw0086 <111766096+jtw0086@users.noreply.github.com>
2024-01-16 14:29:21 -0600

    Added spinner back

--- coachaidge3_0.py
+++ coachaidge3_0.py
@@ -163,11 +163,11 @@ if st.session_state.prompt:
     # Handle run status
     # Check and handle the run status
     while st.session_state.run.status not in ["completed", "max_retries"]:
-
         if st.session_state.run.status == "in_progress":
+            with st.spinner("Thinking ......give me a minute")
             #with st.chat_message("assistant"):
             #    st.write("Thinking ......give me a minute")
-                # time.sleep(15)  # Simulate delay
+                time.sleep(15)  # Simulate delay
             update_run_status()  # Update the status after delay
            
         elif st.session_state.run.status == "failed":
