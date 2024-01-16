c28edf89f637763d7ec3231fba2f10f0d1526729
jtw0086 <111766096+jtw0086@users.noreply.github.com>
2024-01-16 14:27:13 -0600

    Reverted

--- coachaidge3_0.py
+++ coachaidge3_0.py
@@ -165,10 +165,9 @@ if st.session_state.prompt:
     while st.session_state.run.status not in ["completed", "max_retries"]:
 
         if st.session_state.run.status == "in_progress":
-            test = st.chat_input("Thinking ......give me a minute")
             #with st.chat_message("assistant"):
             #    st.write("Thinking ......give me a minute")
-            # time.sleep(15)  # Simulate delay
+                # time.sleep(15)  # Simulate delay
             update_run_status()  # Update the status after delay
            
         elif st.session_state.run.status == "failed":
