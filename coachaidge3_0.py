bef8a421a6094c11aa40032243a294050a2f8cb9
jtw0086 <111766096+jtw0086@users.noreply.github.com>
2024-01-24 22:06:11 -0600

    More button disable testing

--- coachaidge3_0.py
+++ coachaidge3_0.py
@@ -54,6 +54,7 @@ def display_results():
                 for content_part in message.content:
                     message_text = content_part.text.value
                     st.markdown(message_text)
+    st.session_state.button_disabled = False
 
 # Function to find next empty Google Sheets row
 def find_next_empty_row(sheet):
@@ -117,21 +118,27 @@ if st.button('How important are relationships to my overall well being and how d
 
 if st.button('What are quick tips for better time management?', disabled = st.session_state.button_disabled):
     st.session_state.prompt = 'What are quick tips for better time management?'
+    st.session_state.button_disabled = True 
 
 if st.button('How do I combat the negative voice in my head?', disabled = st.session_state.button_disabled):
     st.session_state.prompt = 'How do I combat the negative voice in my head?'
+    st.session_state.button_disabled = True 
 
 if st.button('How can I reduce stress and anxiety caused by sports and school?', disabled = st.session_state.button_disabled):
     st.session_state.prompt = 'How can I reduce stress and anxiety caused by sports and school?'
+    st.session_state.button_disabled = True 
 
 if st.button('How do I stay positive while recovering from an injury?', disabled = st.session_state.button_disabled):
     st.session_state.prompt = 'How do I stay positive while recovering from an injury?'
+    st.session_state.button_disabled = True 
 
 if st.button('Give me 15 possible insecurities I might have about myself that are keeping me from playing at the level I know I am capable of', disabled = st.session_state.button_disabled):
     st.session_state.prompt = 'Give me 15 possible insecurities I might have about myself that are keeping me from playing at the level I know I am capable of?'
+    st.session_state.button_disabled = True 
 
 if st.button('What are limiting beliefs and how are they impacting my life right now?', disabled = st.session_state.button_disabled):
     st.session_state.prompt = 'What are limiting beliefs and how are they impacting my life right now?'
+    st.session_state.button_disabled = True 
 
 typed_input = st.chat_input("How can I help you elevate your life?")
 
