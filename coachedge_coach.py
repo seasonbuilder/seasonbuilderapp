d8a70e7b9386405adc1b7cb47f2ff1e62531e386
seasonbuilder <taylor@seasonbuilder.org>
2024-02-06 09:26:59 -0600

    Update coachedge_coach.py

--- coachedge_coach.py
+++ coachedge_coach.py
@@ -123,22 +123,22 @@ def disable(disable_button):
     st.session_state.input_count += 1
 
 # Create Predefine prompt buttons
-if st.button(button_prompt1, on_click=disable, args=(True,), disabled=st.session_state.get("disabled", False)):
+if st.button(button_prompt1, on_click=disable, args=(False,), disabled=st.session_state.get("disabled", False)):
      st.session_state.prompt = button_prompt1
 
-if st.button(button_prompt2, on_click=disable, args=(True,), disabled=st.session_state.get("disabled", False)):
+if st.button(button_prompt2, on_click=disable, args=(False,), disabled=st.session_state.get("disabled", False)):
      st.session_state.prompt = button_prompt2
 
-if st.button(button_prompt3, on_click=disable, args=(True,), disabled=st.session_state.get("disabled", False)):
+if st.button(button_prompt3, on_click=disable, args=(False,), disabled=st.session_state.get("disabled", False)):
      st.session_state.prompt = button_prompt3
 
-if st.button(button_prompt4, on_click=disable, args=(True,), disabled=st.session_state.get("disabled", False)):
+if st.button(button_prompt4, on_click=disable, args=(False,), disabled=st.session_state.get("disabled", False)):
      st.session_state.prompt = button_prompt4
 
-if st.button(button_prompt5, on_click=disable, args=(True,), disabled=st.session_state.get("disabled", False)):
+if st.button(button_prompt5, on_click=disable, args=(False,), disabled=st.session_state.get("disabled", False)):
      st.session_state.prompt = button_prompt5
 
-if st.button(button_prompt6, on_click=disable, args=(True,), disabled=st.session_state.get("disabled", False)):
+if st.button(button_prompt6, on_click=disable, args=(False,), disabled=st.session_state.get("disabled", False)):
      st.session_state.prompt = button_prompt6
 
 typed_input = st.chat_input("How can I help you elevate your life?", on_submit=disable, args=(True,))
@@ -148,7 +148,13 @@ if typed_input:
     st.session_state.prompt = typed_input 
 
 #Chat input and message creation
-if st.session_state.prompt:
+if st.session_state.input_count >= 2:
+        with st.spinner("Thinking ......give me a minute"):
+            time.sleep(3)
+        with st.chat_message('assistant', avatar='https://static.wixstatic.com/media/b748e0_fb82989e216f4e15b81dc26e8c773c20~mv2.png'):
+            st.write("I'm sorry, I can only support one submission at a time. Could you please re-enter your question?")
+        st.session_state.input_count = 0
+elif st.session_state.prompt and (st.session_state.input_count < 2):
     with st.spinner("Thinking ......give me a minute"):
          time.sleep(3)  # Simulate immediate delay
     if st.session_state.input_count == 1:
@@ -176,12 +182,12 @@ if st.session_state.prompt:
             elif st.session_state.run.status == "failed":
                 st.session_state.retry_error += 1
                 if st.session_state.retry_error < 3:
-                    status_message.write("Run failed, retrying ......")
+                    st.write("Run failed, retrying ......")
                     if retry_button.button('Retry'):
                         update_run_status()
                      
                 else:
-                    status_message.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
+                    st.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
 
             elif st.session_state.run.status != "completed":
                 # Simulate updating the run status
