4d62b28dc9c2fb8f617c3e848641b756907a0547
seasonbuilder <taylor@seasonbuilder.org>
2024-03-03 14:13:22 -0600

    Update coachedge_coach.py

--- coachedge_coach.py
+++ coachedge_coach.py
@@ -183,7 +183,7 @@ elif st.session_state.prompt and (st.session_state.input_count < 2):
             if st.session_state.run.status == "in_progress":
                 with spinner_container:
                     with st.spinner("Thinking ...... please give me 30 seconds"):
-                       time.sleep(15)  # Simulate delay
+                       time.sleep(5)  # Simulate delay
                 update_run_status()  # Update the status after delay
            
             elif st.session_state.run.status == "failed":
