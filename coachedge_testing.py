d3df3573750c782e921237aacb81b3a9bf2c6c27
seasonbuilder <taylor@seasonbuilder.org>
2024-03-10 18:45:47 -0500

    Update coachedge_testing.py

--- coachedge_testing.py
+++ coachedge_testing.py
@@ -63,7 +63,7 @@ School=st.query_params.get_all("school")
 Team=st.query_params.get_all("team")
 role=st.query_params.get_all("role")
 
-additional_instructions = f"The users name is {Fname}. They are a {role} on the {Team} team at the {Schoo})."
+additional_instructions = f"The users name is {Fname}. They are a {role} on the {Team} team at the {School})."
 st.write(additional_instructions)
 
 # Step 1:  Retrieve an Assistant if not already created
