5b67d68ae2cb7f7f2b3250eccdbb3e6b41697056
jtw0086 <111766096+jtw0086@users.noreply.github.com>
2024-03-14 21:38:05 -0500

    Update coachedge_testing.py

--- coachedge_testing.py
+++ coachedge_testing.py
@@ -250,6 +250,5 @@ if st.session_state.prompt:
                 if content.type == 'text':
                     report.append(content.text.value)
                     result = "".join(report).strip()
-                    with st.chat_message('assistant', avatar='https://static.wixstatic.com/media/b748e0_fb82989e216f4e15b81dc26e8c773c20~mv2.png'):
-                        container.markdown(result)
-                container.empty()
+                    #with st.chat_message('assistant', avatar='https://static.wixstatic.com/media/b748e0_fb82989e216f4e15b81dc26e8c773c20~mv2.png'):
+                    container.markdown(result)
