bb0508b40befb4653541798367afc557181b03a9
seasonbuilder <taylor@seasonbuilder.org>
2024-03-24 21:57:00 -0500

    Update coachedge_spiritual.py

--- coachedge_spiritual.py
+++ coachedge_spiritual.py
@@ -285,7 +285,7 @@ School = st.query_params.get("school", "Unknown")
 Team = st.query_params.get("team", "Unknown")
 Role = st.query_params.get("role", "Unknown")
 Language=st.query_params.get("language","Unknown")
-additional_instructions = f"The users name is {Fname}. They are a {Role} on the {Team} team at the {School}. Provide each response in 2 languages... 1)the language that it was asked in and 2) in {Language} if that was not the language the question was asked in."
+additional_instructions = f"The user's name is {Fname}. They are a {Role} on the {Team} team at the {School}. The user's native language is {Language}."
 
 st.markdown("**Ask a question below or select a conversation starter**")    
 
