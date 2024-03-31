6e61fd76c49dc3e3e8f8e0cf5b144cbeb364a567
seasonbuilder <taylor@seasonbuilder.org>
2024-03-31 16:59:43 -0500

    Update coachedge_testing.py

--- coachedge_testing.py
+++ coachedge_testing.py
@@ -278,7 +278,7 @@ if st.session_state.prompt:
         st.markdown(st.session_state.prompt)
     with st.chat_message('assistant', avatar='https://static.wixstatic.com/media/b748e0_fb82989e216f4e15b81dc26e8c773c20~mv2.png'):
         container = st.empty()
-        st.session_state.messages= client.beta.threads.messages.create(
+        st.session_state.thread_messages= client.beta.threads.messages.create(
               st.session_state.thread.id, role="user",content=st.session_state.prompt
         )
         stream = client.beta.threads.runs.create(
@@ -294,4 +294,4 @@ if st.session_state.prompt:
                     delta.append(content.text.value)
                     response = "".join(item for item in delta if item).strip()
                     container.markdown(response)
-    #st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response})
