ef5f61c5545f4fba677eb1a3511f3c306abe3240
seasonbuilder <taylor@seasonbuilder.org>
2024-04-10 15:20:22 -0500

    Update coachedge_spiritual.py

--- coachedge_spiritual.py
+++ coachedge_spiritual.py
@@ -246,6 +246,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
+            
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.markdown("""
