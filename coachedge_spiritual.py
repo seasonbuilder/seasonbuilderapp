aedb4c2bdae9bc1f6acfceb8810fe823820c63f3
seasonbuilder <taylor@seasonbuilder.org>
2024-07-18 11:06:58 -0500

    Update coachedge_spiritual.py

--- coachedge_spiritual.py
+++ coachedge_spiritual.py
@@ -76,7 +76,7 @@ st.session_state.language=st.query_params.get("language","Unknown")
 st.session_state.prompt=st.query_params.get("prompt")
 
 additional_instructions = f"The user's name is {st.session_state.fname}. They are a {st.session_state.role} on the {st.session_state.team} team at the {st.session_state.school} and their native language is {st.session_state.language}. If the response is not given to them in their native language, give a response in their native language too."
-
+st.write(st.session_state.prompt)
 st.markdown("**Ask a question below or select a conversation starter**")    
 
 button_prompt1 = 'How can I be a better Christian example to my team?'
