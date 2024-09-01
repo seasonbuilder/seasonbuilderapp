fe209aa3a17542ee8c5a41caaa01c63e8678193a
seasonbuilder <taylor@seasonbuilder.org>
2024-09-01 12:59:11 -0500

    Update coachedge_testing.py

--- coachedge_testing.py
+++ coachedge_testing.py
@@ -42,7 +42,7 @@ st.session_state.role = st.query_params.get("role", "Unknown")
 st.session_state.language=st.query_params.get("language","Unknown")
 st.session_state.prompt=st.query_params.get("prompt")
 
-additional_instructions = f"The user's name is {st.session_state.fname}. They are a {st.session_state.role} on the {st.session_state.team} team at the {st.session_state.school} and their native language is {st.session_state.language}. If the response is not given to them in their native language, give a response in their native language too."
+additional_instructions = f"The user's name is {st.session_state.fname}. They are a {st.session_state.role} in the sport of {st.session_state.team} at the {st.session_state.school} and their native language is {st.session_state.language}. If the response is not given to them in their native language, give a response in their native language too."
 
 # st.markdown("**Ask a question below or select a conversation starter**")    
 
