3abb42a94cfbfabbace33ed8052bff3f01738e47
seasonbuilder <taylor@seasonbuilder.org>
2024-09-07 09:28:45 -0500

    Update coachaidge4_0.py

--- coachaidge4_0.py
+++ coachaidge4_0.py
@@ -68,9 +68,9 @@ st.markdown("### **Ask a Question or Select a Topic**")
 
 button_prompt1 = 'Mental Health Struggles'
 button_prompt2 = 'Relationships'
-button_prompt3 = 'Getting My Love For the Game Back'
+button_prompt3 = 'Leadership'
 button_prompt4 = 'Reducing Impact of Stress'
-button_prompt5 = 'Staying Positive / Not Being So Negative'
+button_prompt5 = 'Time Management'
 button_prompt6 = 'I Just Need Some Encouragement'
 
 with st.expander("Topics To Get Your Started"):
@@ -82,13 +82,13 @@ with st.expander("Topics To Get Your Started"):
         st.session_state.prompt = 'I am struggling with a relationship. Ask me one question at a time via role play to help me identify my relational challenge and then give me practical advice to improve that relationship.'
 
     if st.button(button_prompt3):
-        st.session_state.prompt = 'I seem to have lost my love to play the game. Ask me one question at a time via role play to help me identify what may be causing this and then help me develop a plan to regain my passion.'
+        st.session_state.prompt = 'I want to grow my leadership skills. Ask me one question at a time via role play to help me identify what areas of leadership I need to work on and then help me develop a plan to grow that area.'
 
     if st.button(button_prompt4):
         st.session_state.prompt = 'I am so stressed out. Ask me one question at a time via role play to help me identify what may be causing my stress (I am open to the possibility that I may be causing it) and then help me develop a plan to cope with it in healthy ways and hopefully reduce the impact stress has on me so I can be happier.'
 
     if st.button(button_prompt5):
-        st.session_state.prompt = 'I listen to negative voices in my head. Ask me one question at a time via role play to help me not accept those negative voices and redirect my self talk to a more positive, inspiring, empowering mindset.'
+        st.session_state.prompt = 'I need help with my time management.  Ask me one questions at a time via roe play to help me identify where I may be mismanaging my time or getting distracted and give me some tips to get better.'
 
     if st.button(button_prompt6):
         st.session_state.prompt = 'I am just down. I need some encouragement. Remind me of some principles from the Principle Bank to encourage me.'
