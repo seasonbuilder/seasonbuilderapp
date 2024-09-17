4a0aa0953b36e0d3814fbfa31da0503f3c3f2710
seasonbuilder <taylor@seasonbuilder.org>
2024-09-17 11:26:39 -0500

    Update coachaidge4_0.py

--- coachaidge4_0.py
+++ coachaidge4_0.py
@@ -46,6 +46,8 @@ st.session_state.language=st.query_params.get("language","Unknown")
 
 additional_instructions = f"The user's name is {st.session_state.fname}. They are a {st.session_state.role} in the sport of {st.session_state.team} at the {st.session_state.school} and their native language is {st.session_state.language}. If the response is not given to them in their native language, give a response in their native language too."
 
+st.write(st.session_state.language)
+
 if st.session_state.language == "Español (Spanish)":  
     st.markdown("### **Haz una pregunta o selecciona un tema**")    
 
