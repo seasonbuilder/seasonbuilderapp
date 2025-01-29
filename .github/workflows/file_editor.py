8c5aac36a7a0047696b1467ab10f629efee02131
seasonbuilder <152528509+seasonbuilder@users.noreply.github.com>
2025-01-29 14:31:22 -0600

    Update file_editor.py

--- .github/workflows/file_editor.py
+++ .github/workflows/file_editor.py
@@ -2,3 +2,15 @@
 
 with open("././coachedge_non_spiritual.py", "a") as f:
     f.write(" ")
+
+with open("././coachaidge4_0.py", "a") as f:
+    f.write(" ")
+
+with open("././coachedge_spiritual.py", "a") as f:
+    f.write(" ")
+
+with open("././coachedge_coach.py", "a") as f:
+    f.write(" ")
+
+with open("././sb_step1.py", "a") as f:
+    f.write(" ")
