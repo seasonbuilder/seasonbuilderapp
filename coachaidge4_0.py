e0961fab179fc612e71e9bf26efff5b9fedba3a3
jtw0086 <jacktwoods01@gmail.com>
2025-01-29 20:34:36 +0000

    Scheduled-Commit

--- coachaidge4_0.py
+++ coachaidge4_0.py
@@ -173,4 +173,4 @@ if st.session_state.prompt:
                             container.markdown(response)
 
     # Save assistant's final response to message history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response}) 
