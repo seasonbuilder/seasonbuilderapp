e80861583f9d09b2a668d41c5ee08894c84a3606
seasonbuilder <taylor@seasonbuilder.org>
2025-04-24 08:55:08 -0500

    Update sb_step1.py

--- sb_step1.py
+++ sb_step1.py
@@ -7,7 +7,7 @@ import requests
 # ====== Adalo Save Function ===========
 def save_answers (answer):
     payload = {
-        "SB_ID": st.session_state.sb_id,
+        "SB_ID": int(st.session_state.sb_id),
         "Answers": answer
     }
     headers = {
