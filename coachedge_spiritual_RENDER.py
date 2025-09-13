9c3624beaefd151987bf03289afde20fb0f75b99
seasonbuilder <taylor@seasonbuilder.org>
2025-09-13 11:56:03 -0500

    Update coachedge_spiritual_RENDER.py

--- coachedge_spiritual_RENDER.py
+++ coachedge_spiritual_RENDER.py
@@ -246,7 +246,8 @@ def get_http_session():
 def get_translations():
     return translations
 
-client, assistant = get_openai()
+with st.spinner("Loading Coach Edge..."):
+    client, assistant = get_openai()
 http = get_http_session()
 lang_translations = get_translations()
 
